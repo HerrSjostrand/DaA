bf7bf0cfafc05b9496edb37e4f3e83365eb5e1c7
HerrSjostrand <bobrdobr1610@outlook.com>
2026-07-02 23:22:16 +0300

    Update 6
    - Добавлена механика прыжка и анимация для него в сцене трипа
    - Исправлен скрипт Movement

--- Assets/Scenes/MainScene/Scripts/Movement.cs
+++ Assets/Scenes/MainScene/Scripts/Movement.cs
@@ -6,16 +6,19 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     public SpriteRenderer sr;
     public float moveSpeed;
+    public float jumpForce;
 
     private Vector2 moveDirection;
 
     public InputActionReference move;
 
     private Animator animator;
+    private LayerMask layerMask;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        layerMask = LayerMask.GetMask("Ground");
     }
 
     void Update()
@@ -35,10 +38,40 @@ public class Movement : MonoBehaviour
         {
            animator.SetBool("IsWalking", false); 
         }
+        if (!IsGrounded())
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsWalking", false);
+        }
+        if (IsGrounded())
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
+
+        Debug.Log(IsGrounded());
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.down), 1.1f, layerMask);
     }
 }
